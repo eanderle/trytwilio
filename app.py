4511c93ab5c08841a9bb1adec88c24bc27230d21
Oscar Sanchez <oscar@twilio.com>
2012-07-16 13:36:40 -0700

    changed dependencies and added template folder

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import os
 from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask import request
+
+from twilio import twiml
+from twilio.util import TwilioCapability
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-	return 'Test'
+  params=[]
+  return render_template('index.html', params=params)
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
 
