9d6991d4ea73241d32de33a73af068b29316768c
Oscar Sanchez <oscar@twilio.com>
2012-07-16 15:38:11 -0700

    added xml checking for twiml

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def hello():
   params=[]
   return render_template('index.html', params=params)
 
+@app.route('/test', methods=['GET', 'POST'])
+def xmlcheck():
+  return render_template('test.html')
+
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port)
