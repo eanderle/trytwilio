7a7c160d8ebe97680353c0297dd457cd41eba199
Chris Wendel <chriwend@umich.edu>
2012-07-17 18:01:52 -0400

    commiting to trytwiliotest

--- app.py
+++ app.py
@@ -4,24 +4,29 @@ import datetime
 from flask import Flask, request, render_template
 from urllib import urlencode
 from twilio.rest import TwilioRestClient
+from twilio.util import TwilioCapability
 from mongokit import Document, Connection
 
-class OutboundCall(Document):
-  __collection__ = 'outboundCalls'
-  __database__ = 'heroku_app5944498'
-  structure = { 'number': basestring,
-                'timestamp': datetime.datetime
-  }
-  required_fields = ['number', 'timestamp']
-  default_values = { 'timestamp': datetime.datetime.utcnow()}
-
 app = Flask(__name__)
+#
+#class OutboundCall(Document):
+#  __collection__ = 'outboundCalls'
+#  __database__ = 'heroku_app5944498'
+#  structure = { 'number': basestring,
+#                'timestamp': datetime.datetime
+#  }
+#  required_fields = ['number', 'timestamp']
+#  default_values = { 'timestamp': datetime.datetime.utcnow()}
+#
+#app = Flask(__name__)
+#
+#client = TwilioRestClient("ACefb267919ab7c793e889ce40b8db2506","6cb0a97591eaf94ca237572fe4472458")
 
-client = TwilioRestClient()
+#client = TwilioRestClient("ACefb267919ab7c793e889ce40b8db2506","6cb0a97591eaf94ca237572fe4472458")
 
-connection = Connection(os.environ.get('MONGOLAB_URI'))
-connection.register([OutboundCall])
-db = connection['heroku_app5944498']
+#connection = Connection(os.environ.get('MONGOLAB_URI'))
+#connection.register([OutboundCall])
+#db = connection['heroku_app5944498']
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
@@ -35,6 +40,22 @@ def xmlcheck():
 @app.route('/testXml', methods=['GET', 'POST'])
 def xmlcheck():
   return render_template('testValidation.html')
+
+@app.route('/testClient', methods=['GET','POST'])
+def testClient():
+	application_sid = "AP256035c642dcf6ad2f82119f86e4ea35"
+
+	capability = TwilioCapability("ACefb267919ab7c793e889ce40b8db2506","6cb0a97591eaf94ca237572fe4472458")
+	capability.allow_client_outgoing(application_sid)
+	token = capability.generate()
+
+	return render_template("client.html", token=token)
+	#return render_template('client.html', token=token)
+	
+
+@app.route('/client/getTwiml')
+def requestTwiml():
+	return "<Response><Say>This is a test</Say></Response>"
 
 @app.route('/requestCall', methods=['GET', 'POST'])
 def requestCall():
