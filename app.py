aae0a49dad473c93019057d8aeb08c2b7854667b
Eric Anderle <anderle@twilio.com>
2012-07-18 14:21:46 -0700

    started work on multiple twiml bodies for gather

--- app.py
+++ app.py
@@ -78,6 +78,12 @@ def requestTwiml():
     #sys.stderr.write(e)
     return "<Response><Say>Welcome to Twilio this is an error </Say></Response>"
 
+@app.route('/handleInput')
+def requestTwimlForGather():
+  s = request.values['twimlBody' + request.values['Digits']]
+  sys.stderr.write(s + '\n')
+  return s
+
 @app.route('/requestCall', methods=['GET', 'POST'])
 def requestCall():
   try:
@@ -99,6 +105,15 @@ def requestCall():
       ip = request.remote_addr
       twimlBody = request.values['twimlBody']
 
+      url = 'http://trytwilio.herokuapp.com/requestCall?' + urlencode({'twimlBody':twimlBody})
+
+      if request.values['verb'].lower() == 'gather':
+        url = 'http://trytwilio.herokuapp.com/handleInput?'
+        for c in '0123456789#*':
+          s = 'twimlBody' + c
+          if s in request.values:
+            url += urlencode({s:request.values[s]}) + '&'
+
       # Clean up old entries and make sure this number hasn't been called too much
       d = datetime.datetime.utcnow() - datetime.timedelta(days = 1)
       connection.OutboundCall.collection.remove({'$and': [{'$or': [{'number': toNumber}, {'ip': ip}]},
@@ -111,8 +126,7 @@ def requestCall():
 
       sys.stderr.write('Making Twilio Rest Request...\n')
       client.calls.create(to=toNumber, from_=FROM_NUMBER, 
-        url='http://trytwilio.herokuapp.com/requestCall?' + urlencode({'twimlBody':twimlBody}),
-        method='GET')
+        url=url, method='GET')
 
       sys.stderr.write('Creating call in db...\n')
       call = connection.OutboundCall()
